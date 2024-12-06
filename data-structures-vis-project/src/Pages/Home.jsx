81836066187e066f42079cd3392ad01f6643a711
JoshuaHM-p4 <joshuahm.2004@gmail.com>
2024-12-06 22:34:41 +0800

    feat: refactor homepage grid item into standalone as HomepageItem component and update Homepage

--- data-structures-vis-project/src/Pages/Home.jsx
+++ data-structures-vis-project/src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HomepageItem from '../components/HomepageComponents/HomepageItem.jsx';
 
 const Home = () => {
   const navLinks = [
@@ -19,7 +20,7 @@ const Home = () => {
         <h1 className="text-5xl font-bold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">Welcome to Data Structures Visualization</h1>
         <div className="grid grid-cols-4 gap-4">
           {navLinks.map((navLink, index) => (
-            <Link key={index} className="bg-slate-500 h-40 w-40 rounded shadow flex items-center justify-center text-center hover:shadow hover:translate-y-2 transition-all " to={navLink.Link}>{navLink.title}</Link>
+            <HomepageItem key={index} item={navLink} />
           ))}
         </div>
       </div>
