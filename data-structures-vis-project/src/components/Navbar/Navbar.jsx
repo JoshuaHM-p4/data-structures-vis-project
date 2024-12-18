287a0a7cd8baf4486af7f4e6bd47b32f62863071
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-12-19 00:50:52 +0800

    feat: update Navbar to reflect active link based on current route

--- data-structures-vis-project/src/components/Navbar/Navbar.jsx
+++ data-structures-vis-project/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBurger } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isActive, setIsActive] = useState("Home");
+  const location = useLocation();
+  const [isActive, setIsActive] = useState(location.pathname);
+
+  useEffect(() => {
+    setIsActive(location.pathname);
+  }, [location]);
 
   const navLinks = [
-    { Link: '/', title: 'Home' },
-    { Link: '/tic-tac-toe', title: 'Tic Tac Toe' },
-    { Link: '/stacks', title: 'Stacks' },
-    { Link: '/queue', title: 'Queue' },
-    { Link: '/binary-tree-traversal', title: 'Binary Tree Traversal' },
-    { Link: '/binary-search-tree', title: 'Binary Search Tree' },
-    { Link: '/towers-of-hanoi', title: 'Towers of Hanoi' },
-    { Link: '/sorting', title: 'Sorting' },
+    { link: '/', title: 'Home' },
+    { link: '/tic-tac-toe', title: 'Tic Tac Toe' },
+    { link: '/stacks', title: 'Stacks' },
+    { link: '/queue', title: 'Queue' },
+    { link: '/binary-tree-traversal', title: 'Binary Tree Traversal' },
+    { link: '/binary-search-tree', title: 'Binary Search Tree' },
+    { link: '/towers-of-hanoi', title: 'Towers of Hanoi' },
+    { link: '/sorting', title: 'Sorting' },
   ];
 
   return (
@@ -37,11 +42,11 @@ const Navbar = () => {
           {navLinks.map((navLink, index) => (
             <li key={index}>
               <Link
-                className={`${isActive === navLink.title ? 'text-cyan-400 ' : ''} text-neutral-100 flex text-center align-middle justify-center lg:inline min-w-fit lg:m-0 bg-stone-800 lg:bg-transparent rounded-lg p-2 transition-all duration-300 ease-in-out hover:text-cyan-400 hover:bg-stone-700 active:opacity-80`}                
-                to={navLink.Link}
+                className={`${isActive === navLink.link ? 'text-cyan-400 ' : ''} text-neutral-100 flex text-center align-middle justify-center lg:inline min-w-fit lg:m-0 bg-stone-800 lg:bg-transparent rounded-lg p-2 transition-all duration-300 ease-in-out hover:text-cyan-400 hover:bg-stone-700 active:opacity-80`}                
+                to={navLink.link}
                 onClick={() => {
                   setIsMenuOpen(false);
-                  setIsActive(navLink.title);
+                  setIsActive(navLink.link);
                 }}
               >
                 {navLink.title}
