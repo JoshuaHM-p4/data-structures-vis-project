cd5cb31eadca27fa5a8609f180858a2c886cb1df
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-12-19 23:22:34 +0800

    feat: enhance Modal component to focus input on arrival and support car selection for departure

--- data-structures-vis-project/src/components/StackQueueModal/Modal.jsx
+++ data-structures-vis-project/src/components/StackQueueModal/Modal.jsx
@@ -1,21 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const Modal = ({ isOpen, onClose, onSubmit, plateNumber, setPlateNumber, mode }) => {
-  if (!isOpen) return null;
+const Modal = ({ isModalOpen, onClose, onSubmit, plateNumber, setPlateNumber, mode, cars }) => {
+  // Reference to the input field
+	const inputRef = useRef(null);
+	
+	// Focus on the input field when the modal is open
+  useEffect(() => {
+    if (isModalOpen && mode === 'arrival' && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isModalOpen, mode]);
+
+	// Return null if the modal is not open
+  if (!isModalOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white p-5 rounded shadow-lg">
         <h2 className="text-xl mb-4 text-black">Enter Plate Number</h2>
         <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            value={plateNumber}
-            onChange={(e) => setPlateNumber(e.target.value)}
-            placeholder="Plate Number"
-            className="px-4 py-2 border rounded mb-4 w-full text-black"
-            required
-          />
+          {mode === 'arrival' ? (
+            <input
+              type="text"
+              value={plateNumber}
+              onChange={(e) => setPlateNumber(e.target.value)}
+              placeholder="Plate Number"
+              className="px-4 py-2 border rounded mb-4 w-full text-black"
+              required
+              ref={inputRef}
+            />
+          ) : (
+            <div className="flex items-center gap-2">
+              <select
+                value={plateNumber}
+                onChange={(e) => setPlateNumber(e.target.value)}
+                className="px-4 py-2 border rounded mb-4 w-[251.33px] text-black"
+                required
+              >
+                <option value="" disabled>Select Car</option>
+                {cars.map((car, index) => (
+                  <option key={index} value={car.plateNumber}>
+                    {car.plateNumber}
+                  </option>
+                ))}
+              </select>
+            </div>
+          )}
           <div className="flex justify-end gap-2">
             <button
               type="button"
