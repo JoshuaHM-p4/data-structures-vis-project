4f4110b1f558ff6cce184ef174dd1da66a2433e5
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-12-28 17:38:36 +0800

    feat: update Home and Carousel components for improved layout and responsiveness

--- data-structures-vis-project/src/Pages/Home.jsx
+++ data-structures-vis-project/src/Pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   ];
 
   return (
-    <div className="bg-slate-700 md:h-full p-2 sm:p-4">
+    <div className="bg-slate-700 h-full p-2 sm:p-4">
       <div className="h-full w-full flex flex-col items-center justify-center text-center gap-3">
         <h1 className="text-2xl font-bold no-text-border bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
           Welcome to Data Structures Visualization
