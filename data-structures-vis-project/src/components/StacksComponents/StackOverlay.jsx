ad4e4c76dd696a528cacc9be7fcdddb18a94f35a
JoshuaHM-p4 <joshuahm.2004@gmail.com>
2024-12-30 20:26:58 +0800

    feat: enhance StackOverlay component to display car plate number and type

--- data-structures-vis-project/src/components/StacksComponents/StackOverlay.jsx
+++ data-structures-vis-project/src/components/StacksComponents/StackOverlay.jsx
@@ -12,10 +12,12 @@ const StackOverlay = ({ car, colorMap }) => {
   };
 
   return (
-    <div className='p-5 absolute bottom-3 right-0 text-center flex flex-col'>
+    <div className='p-5 absolute bottom-3 gap-1 right-0 text-center flex flex-col'>
       <p>Recent Car:</p>
       <div className='h-20'></div>
       <img src={getImagePath(car?.type, car?.color, car?.isUtility)} alt={car?.type} className='absolute top-0 left-20 w-40 h-40 mx-auto p-[-5rem]' />
+      <p className='border w-fit mx-auto px-4'>{car.plateNumber}</p>
+      <p>{!car?.isUtility ? car?.color + ' ' : ''}{car?.type}</p>
       <p>Arrivals: {car?.arrivalCount} | Departures: {car?.departureCount}</p>
     </div>
   );
