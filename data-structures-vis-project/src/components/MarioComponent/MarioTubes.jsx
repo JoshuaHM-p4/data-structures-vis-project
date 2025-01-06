0b24897f4be238272dd73b6c646a7da8e7fa8372
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-01-06 13:45:58 +0800

    feat: enhance Tube component

--- data-structures-vis-project/src/components/MarioComponent/MarioTubes.jsx
+++ data-structures-vis-project/src/components/MarioComponent/MarioTubes.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../../styles/tube.css';
 
-const Tube = ({ height = '1', comparing = false, sorted = false, style }) => {
+const Tube = ({ height = '1', comparing = false, sorted = false, reference = false, style }) => {
   return (
-    <div className={`tube ${comparing ? 'comparing' : ''} ${sorted ? 'sorted' : ''}`} style={{ ...style, height }}></div>
+    <div className={`tube ${comparing ? 'comparing' : ''} ${sorted ? 'sorted' : ''} ${reference ? 'reference' : ''}`} style={{ ...style, height }}></div>
   );
 };
 
