526a4abd6ecfd4a550dc6fca163b82702409ac49
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-01-08 13:43:49 +0800

    feat: enhance Tooltip component with slider position support and transition effects

--- data-structures-vis-project/src/components/Tooltip/Tooltip.jsx
+++ data-structures-vis-project/src/components/Tooltip/Tooltip.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Tooltip = ({ children, text, optionalText = "", position = "top", alwaysVisible = false, addedStyle=''}) => {
+const Tooltip = ({ children, text, optionalText, sliderPosition, position = "top", alwaysVisible = false , addedStyle=''}) => {
   let positionClasses;
   let arrowClasses;
 
@@ -26,11 +26,17 @@ const Tooltip = ({ children, text, optionalText = "", position = "top", alwaysVi
       arrowClasses = "left-1/2 transform -translate-x-1/2 -bottom-1";
   }
 
+  const sliderClasses = sliderPosition ? 'transition-none' : 'transition-all duration-300 ease-in-out ';
+
   return (
     <div className="relative inline-block group">
       {children}
-      <div className={`absolute z-40 ${alwaysVisible ? 'visible opacity-50 group-hover:z-50 group-hover:opacity-100' : `invisible opacity-0 group-hover:visible group-hover:opacity-100`}  ${positionClasses} transition-all duration-300 ease-in-out` }>
-        <div className={`relative px-4 ${addedStyle} py-2 text-[0.625rem] text-white bg-gray-900 rounded-lg backdrop-blur-sm bg-opacity-90 border border-gray-700/50 shadow-xl w-60 text-center`}>
+      <div className={`absolute z-40 ${alwaysVisible ? 'visible opacity-50 group-hover:z-50 group-hover:opacity-100' : `invisible opacity-0 group-hover:visible group-hover:opacity-100`}  
+      ${positionClasses} 
+      ${sliderClasses}`}
+      style={{left: sliderPosition}}
+    >
+        <div className={`relative px-4 ${addedStyle} py-2 text-[0.625rem] text-white bg-gray-900 rounded-lg backdrop-blur-sm bg-opacity-90 border border-gray-700/50 shadow-xl ${sliderPosition ? 'w-auto' : 'w-60'} text-center`}>
           <div className="flex items-center flex-col">
             <span>{text}</span>
             <span>{optionalText}</span>
