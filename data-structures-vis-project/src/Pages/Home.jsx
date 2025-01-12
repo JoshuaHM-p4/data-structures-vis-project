bd9cfdddd5d63945591f80280e9653cbbe6035d2
Gerald Berongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2025-01-13 02:00:38 +0800

    feat: add sound effect and toggle button for carousel in Home component

--- data-structures-vis-project/src/Pages/Home.jsx
+++ data-structures-vis-project/src/Pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import Carousel from '../components/Carousel/Carousel.jsx';
+import { useState } from 'react';
+
+import useSound from '../hooks/useSound.js';
+import select from '../assets/sounds/select.mp3';
+
 
 const Home = () => {
+  const [openCarousel, setOpenCarousel] = useState(false);
+
+  const { playSound } = useSound();
+  const selectSound = () => { playSound(select) };
+
   const navLinks = [
     { Link: '/tic-tac-toe', title: 'Tic Tac Toe', description: 'Array application with the use of column and row coordinates.' },
     { Link: '/stacks', title: 'Stacks', description: 'Simulation of a Stack using the 10-vehicle CEA garage as the model.' },
@@ -14,11 +24,19 @@ const Home = () => {
 
   return (
     <div className="bg-slate-700 h-full p-2 sm:p-4">
-      <div className="h-full w-full flex flex-col items-center justify-center text-center gap-3">
-        <h1 className="text-2xl font-bold no-text-border bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+      <div className="h-full w-full flex flex-col items-center justify-center text-center gap-3 relative">
+        {/* <h1 className="text-2xl font-bold no-text-border bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
           Welcome to Data Structures Visualization
-        </h1>
-        <Carousel items={navLinks} />
+        </h1> */}
+      <button className="nes-btn is-primary absolute top-0 left-0" 
+          onClick={() => {
+          setOpenCarousel(prevState => !prevState);
+          selectSound();
+      }}>
+        Draw Cards
+      </button>        
+
+      {openCarousel && <Carousel items={navLinks} />}
       </div>
     </div>
   );
